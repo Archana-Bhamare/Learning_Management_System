7a72587bde99fddf62fc9dcc017faff648d11450
Archana-Bhamare <archusonar2@gmail.com>
2021-01-27 22:14:22 +0530

    Added : Register Models

---
+++ Learning_System/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from Learning_System.models import *
+
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Education)
+admin.site.register(Mentor)
+admin.site.register(Performance)
+
