265ce10b725e70c19d02e30ece8a24e433ce8b1b
Archana-Bhamare <archusonar2@gmail.com>
2021-01-30 10:51:34 +0530

    Modify : Set Forgot password url

--- User/urls.py
+++ User/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('activate/<surl>/', views.activate, name="activate"),
     path('login/', views.UserLoginAPI.as_view(), name='login'),
     path('logout/', views.UserLogoutAPI.as_view(), name='logout'),
+    path('forgotpassword/', views.ForgotPasswordAPI.as_view(), name='forgotpassword'),
 ]
