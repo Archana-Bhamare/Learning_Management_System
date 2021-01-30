9617f286bcb83d428d932739550ec8c8febef39c
Archana-Bhamare <archusonar2@gmail.com>
2021-01-30 10:33:12 +0530

    Refactor :  refactor the user registration seril

--- User/serializer.py
+++ User/serializer.py
@@ -6,8 +6,4 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-
-    def validate(self, data):
-        if User.objects.filter(email=data['email']).exists():
-            raise serializers.ValidationError("This Email already Exist, Please enter another Email")
-        return data
+c
