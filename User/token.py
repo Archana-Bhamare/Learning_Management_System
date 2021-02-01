acae521158da3e6cd141d4cd1b7efd33556df0e7
Archana-Bhamare <archusonar2@gmail.com>
2021-02-01 15:38:10 +0530

    Added : Add verifytoken function

--- User/token.py
+++ User/token.py
@@ -1,14 +1,24 @@
 import datetime
 import jwt
+from rest_framework.response import Response
 from LMSystem.settings import SECRET_KEY
 
 
-def token_activation(username, password=None):
-    data = {
-        'username': username,
-        'password': password,
-        'exp': datetime.datetime.now() + datetime.timedelta(days=1)
-    }
+class TokenAuthentication:
+    @staticmethod
+    def token_activation(username, password):
+        pay_load = {
+            'username': username,
+            'password': password,
+            'exp': datetime.datetime.now() + datetime.timedelta(days=1)
+        }
+        token = jwt.encode(pay_load, SECRET_KEY)
+        return token
 
-    token = jwt.encode(data, SECRET_KEY, algorithm="HS256").decode('utf-8')
-    return token
+    @staticmethod
+    def verifyToken(token, secretkey=SECRET_KEY):
+        try:
+            verificationStatus = jwt.decode(token, key=secretkey, algorithms='HS256')
+            return verificationStatus
+        except jwt.ExpiredSignature:
+            return Response("Token Expired")
