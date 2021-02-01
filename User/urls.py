19e052ac43b032d0f02d314f02e02e7acaaf8bdc
Archana-Bhamare <archusonar2@gmail.com>
2021-02-01 15:40:59 +0530

    Added : Set reset and change password APIs url

--- User/urls.py
+++ User/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('register/', views.UserRegisterAPI.as_view(), name='register'),
-    path('activate/<surl>/', views.activate, name="activate"),
     path('login/', views.UserLoginAPI.as_view(), name='login'),
+    path('changepassword/', views.ChangePasswordAPI.as_view(), name='changepassword'),
     path('logout/', views.UserLogoutAPI.as_view(), name='logout'),
     path('forgotpassword/', views.ForgotPasswordAPI.as_view(), name='forgotpassword'),
+    #path('resetpassword/', views.ResetPasswordAPI.as_view(), name='resetpassword'),
 ]
