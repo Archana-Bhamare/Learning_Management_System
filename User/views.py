70288e2cbd45b67b69aaf5257468981a05cf9f27
Archana-Bhamare <archusonar2@gmail.com>
2021-02-01 15:35:15 +0530

    Modify : create reset and change password API

--- User/views.py
+++ User/views.py
@@ -1,24 +1,19 @@
 import logging
-import jwt
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout
 from django.db.models import Q
 from django.shortcuts import redirect
-from django_short_url.models import ShortURL
-from django_short_url.views import get_surl
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from django.core.mail import EmailMessage
 from django.contrib.sites.shortcuts import get_current_site
-from django.template.loader import render_to_string
-from LMSystem import settings
 from LMSystem.settings import file_handler
+from User.mailconfigure import Email
 from User.models import User
-from User.serializer import UserRegisterSerializer, UserLoginSerializer, \
-    ForgotPasswordFormSerializer
+from User.serializer import UserRegisterSerializer, UserLoginSerializer, ChangePasswordSerializer, ForgotPasswordSerializer, ResetPasswordSerializer
 from rest_framework_jwt.settings import api_settings
+from django.contrib.auth.hashers import make_password, check_password
 
-from User.token import token_activation
+from User.token import TokenAuthentication
 
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
@@ -59,66 +54,33 @@ class UserRegisterAPI(GenericAPIView):
             return Response("Email id already exist",
                             status=status.HTTP_400_BAD_REQUEST)
         else:
+            passwd = make_password(password)
             user = User.objects.create(first_name=first_name, last_name=last_name,
                                        username=username, email=email,
-                                       password=password, mob_no=mob_no)
+                                       password=passwd, mob_no=mob_no, role=role)
+            user.save()
             try:
-                if role == "Engineer":
-                    user.is_active = False
-                    user.save()
-                    payload = jwt_payload_handler(user)
-                    token = jwt_encode_handler(payload)
-                    url = str(token)
-                    surl = get_surl(url)
-                    short_token = surl.split("/")
-                    message = "You are register successfully"
-                    mail_message = render_to_string('email_validation.html', {
-                        'user': user,
-                        'domain': get_current_site(request).domain,
-                        'surl': short_token[2]
-                    })
-                    recipients_email = email
-                    email = EmailMessage(message, mail_message, to=[recipients_email])
-                    email.send()
-                    logger.info("Mail Successfully send to the Engineer")
-                    return Response('Mail Successfully send to the Engineer',
-                                    status=status.HTTP_200_OK)
-                if role == "Mentor":
-                    user.save()
-                    logger.info("Mentor Register Successfully")
-                    return Response("Successfully register Mentor",
-                                    status=status.HTTP_200_OK)
+                if role == "Engineer" or role == "Mentor":
+                    token = TokenAuthentication.token_activation(username=request.data['username'],
+                                                                 password=request.data['password'])
+                    data = {
+                        'name': f"{request.data['first_name']} {request.data['last_name']}",
+                        'username': request.data['username'],
+                        'password': request.data['password'],
+                        'role': request.data['role'],
+                        'email': request.data['email'],
+                        'site': get_current_site(request).domain,
+                        'token': token
+                    }
+                    Email.sendEmail(Email.email_validation(data))
+                    return Response({'response': f"A new {user.role} is added successfully"},
+                                    status=status.HTTP_201_CREATED)
             except:
                 logger.error("Something went Wrong")
                 return Response("Something went wrong",
                                 status=status.HTTP_400_BAD_REQUEST)
             logger.info("Admin Register Successfully")
             return Response("Admin Register Successfully", status=status.HTTP_200_OK)
-
-
-def activate(request, surl):
-    """
-        @param request: once the account verification link is clicked by user this will
-take that request
-        @return: it will redirect to login page
-    """
-    try:
-        tokenobject = ShortURL.objects.get(surl=surl)
-        token = tokenobject.lurl
-        decode = jwt.decode(token, settings.SECRET_KEY)
-        username = decode['username']
-        user = User.objects.get(username=username)
-        if user is not None:
-            user.is_active = True
-            user.save()
-            logger.info("successfully activate your account")
-            return redirect('login')
-        else:
-            logger.error("User not valid")
-            return redirect('login')
-    except KeyError:
-        logger.error("Key Error")
-        return Response("Key Error")
 
 
 class UserLoginAPI(GenericAPIView):
@@ -136,14 +98,24 @@ class UserLoginAPI(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         username = serializer.data.get('username')
         password = serializer.data.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            logger.info("You are logged in Successfully")
-            return Response("You are logged in Successfully",
-                            status=status.HTTP_200_OK)
-        logger.error("Invalid User")
-        return Response("Invalid User", status=status.HTTP_401_UNAUTHORIZED)
+        try:
+            user = User.objects.get(username=username)
+            password_match = check_password(password, user.password)
+            if user and password_match:
+                if user.first_login:
+                    token = request.GET.get('token')
+                    if TokenAuthentication.verifyToken(token):
+                        login(request, user)
+                        user.save()
+                        return redirect('changepassword')
+                elif not user.first_login:
+                    login(request, user)
+                    logger.info("You are logged in Successfully")
+                return Response("You are logged in Successfully",
+                                status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            logger.error("Invalid User")
+            return Response("Invalid User", status=status.HTTP_401_UNAUTHORIZED)
 
 
 class UserLogoutAPI(GenericAPIView):
@@ -166,41 +138,67 @@ class UserLogoutAPI(GenericAPIView):
                             status=status.HTTP_400_BAD_REQUEST)
 
 
+class ChangePasswordAPI(GenericAPIView):
+    """ This API is used to change the user password"""
+    serializer_class = ChangePasswordSerializer
+
+    def put(self, request):
+        """
+        This function used to change old password with new password
+        :param request: old password
+        :return: Change the old password with new password
+        """
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        password = serializer.data.get('password')
+        if check_password(password, request.user.password):
+            request.user.set_password(serializer.data.get('new_password'))
+            request.user.save()
+            logger.info("Password Change Successfully")
+            return Response("Password Change Successfully", status=status.HTTP_200_OK)
+        logger.error("Password Mismatch")
+        return Response("Password Mismatch", status=status.HTTP_400_BAD_REQUEST)
+
 class ForgotPasswordAPI(GenericAPIView):
-    """ This API is used for when user forgot password"""
-    serializer_class = ForgotPasswordFormSerializer
+    serializer_class = ForgotPasswordSerializer
 
     def post(self, request):
+        """This API is used to send reset password link to user email id
+        @param request: user email id
+        @return: password reset link with jwt token
         """
-        If user forgot his password then he can reset his password here
-        @param request: user request
-        @return: reset password
-        """
-        email = request.data['email']
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
         try:
-            user = User.objects.filter(email=email)
-            if user.count() == 0:
-                logger.error("Not Found mail in database")
-                return Response("Not Found mail in database")
-            else:
-                username = user.values()[0]["username"]
-                current_site = get_current_site(request)
-                domain_name = current_site.domain
-                token = token_activation(username=username)
-                url = str(token)
-                surl = get_surl(url)
-                short_token = surl.split('/')
-                mail_subject = " Forgotten Password reset"
-                msg = render_to_string('reset_email.html', {
-                    'user': user,
-                    'domain': domain_name,
-                    'surl': short_token[2]
-                })
-                recipients = email
-                email = EmailMessage(mail_subject, msg, to=[recipients])
-                email.send()
-                logger.info('Reset Password link sent')
-                return Response('Please check your email address to reset password')
-        except KeyError:
-            logger.error("Key Error")
-            return Response("Key error")
+            user = User.objects.get(email=serializer.data.get('email'))
+        except User.DoesNotExist:
+            return Response("Email does not exist", status=status.HTTP_404_NOT_FOUND)
+        email_data = {
+            'user': user,
+            'site': get_current_site(request).domain,
+            'token': TokenAuthentication.token_activation(username=user.username, password=user.password)
+        }
+        Email.sendEmail(Email.reset_password(email_data))
+        logger.info('Reset Password link sent')
+        return Response("Please check your email address to reset password", status=status.HTTP_200_OK)
+
+# class ResetPasswordAPI(GenericAPIView):
+#     serializer_class = ResetPasswordSerializer
+#     def post(self,request):
+#         token = request.GET.get('token')
+#         password = request.data['password']
+#         confirm_password = request.data['confirm_password']
+#         if password == "" or confirm_password == "":
+#             logger.error("you can not put empty field")
+#             return Response("you can not put empty field")
+#         if password == confirm_password:
+#             userdata = TokenAuthentication.verifyToken(token)
+#             if userdata:
+#                 user = userdata.get('username')
+#                 user.set_password(password)
+#                 user.save()
+#                 logger.info("successful reset password")
+#                 return Response("successful reset password")
+#         else:
+#             logger.error("Password Mismatch")
+#             return Response("password mismatch")
