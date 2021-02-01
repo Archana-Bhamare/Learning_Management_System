8ac34dbb401be4455e7ba0af71b123034ca6e32f
Archana-Bhamare <archusonar2@gmail.com>
2021-02-01 15:37:26 +0530

    Modify : Add change password and reset password serializer

--- User/serializer.py
+++ User/serializer.py
@@ -1,18 +1,41 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
+
 from User.models import User
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'confirm_password', 'mob_no', 'role']
+
 
 class UserLoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password']
 
-class ForgotPasswordFormSerializer(serializers.ModelSerializer):
+
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    new_password = serializers.CharField(max_length=15)
+
+    class Meta:
+        model = User
+        fields = ['password', 'confirm_password', 'new_password']
+
+    def validate(self, data):
+        if data.get('password') != data.get('confirm_password'):
+            raise serializers.ValidationError("Password Does not Match!")
+        return data
+
+class ForgotPasswordSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email']
+
+class ResetPasswordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['password', 'confirm_password']
+
