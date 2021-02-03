bd658d7d2a09692c0fdc3b2d137dc838a59782b3
Archana-Bhamare <archusonar2@gmail.com>
2021-02-03 16:37:25 +0530

    Added : set Learning_Management url

--- Learning_System/urls.py
+++ Learning_System/urls.py
@@ -34,5 +34,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('User/', include('Authentication.urls')),
+    path('Management/', include('learning_management.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
