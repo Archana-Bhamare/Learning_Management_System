62786fe1523d59e5defcd23d409b9d9189b9e1de
Archana-Bhamare <archusonar2@gmail.com>
2021-02-03 16:35:34 +0530

    Added : Add signal for User role

---
+++ learning_management/signals.py
@@ -0,0 +1,15 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+from Authentication.models import User
+from learning_management.models import Student, Education, Mentor
+
+
+@receiver(post_save, sender=User)
+def create_student_details(sender, instance, created, **kwargs):
+    if created:
+        if instance.role == 'Engineer':
+            student = Student.objects.create(student=instance)
+            Education.objects.create(student=student)
+        elif instance.role == 'Mentor':
+            Mentor.objects.create(mentor=instance)
