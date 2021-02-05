8e7481466695bddce5246925982ad603a9d5dbdb
Archana-Bhamare <archusonar2@gmail.com>
2021-02-05 09:57:48 +0530

    Added : set studentmentor url

--- learning_management/urls.py
+++ learning_management/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('addcourse', views.AddCourseAPI.as_view(), name='addcourse'),
     path('updatecourse/<int:course_id>', views.UpdateCourseAPI.as_view(),name='updatecourse'),
     path('updatestudent/<int:student_id>', views.UpdateStudentDetailsAPI.as_view(), name='updatestudent'),
+    path('updateeducation/',views.UpdateStudentEducationAPI.as_view(), name='updateeducation'),
     path('updatementor/<int:mentor_id>', views.UpdateMentorDetailsAPI.as_view(), name='updatementor'),
     path('displaymentor/',views.DisplayMentorDetailsAPI.as_view(),name='displyamentor'),
+    path('studentmentor/', views.StudentMentorAPI.as_view(), name='studentmentor')
 ]
