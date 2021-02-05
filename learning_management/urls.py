2d8b64400c37b1205fd17caf851d46b60addb71f
Archana-Bhamare <archusonar2@gmail.com>
2021-02-05 17:46:29 +0530

    Refactor : refactor the urls name

--- learning_management/urls.py
+++ learning_management/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('addcourse', views.AddCourseAPI.as_view(), name='addcourse'),
-    path('updatecourse/<int:course_id>', views.UpdateCourseAPI.as_view(), name='updatecourse'),
-    path('updatestudent/<int:student_id>', views.UpdateStudentDetailsAPI.as_view(), name='updatestudent'),
-    path('updateeducation/', views.UpdateStudentEducationAPI.as_view(), name='updateeducation'),
-    path('updatementor/<int:mentor_id>', views.UpdateMentorDetailsAPI.as_view(), name='updatementor'),
-    path('displaymentor/', views.DisplayMentorDetailsAPI.as_view(), name='displyamentor'),
-    path('studentmentor/', views.StudentMentorAPI.as_view(), name='studentmentor')
+    path('update-course/<int:course_id>', views.UpdateCourseAPI.as_view(),name='update-course'),
+    path('update-student/<int:student_id>', views.UpdateStudentDetailsAPI.as_view(), name='update-student'),
+    path('update-education/',views.UpdateStudentEducationAPI.as_view(), name='update-education'),
+    path('update-mentor/<int:mentor_id>', views.UpdateMentorDetailsAPI.as_view(), name='update-mentor'),
+    path('display-mentor/',views.DisplayMentorDetailsAPI.as_view(),name='display-mentor'),
+    path('student-mentor/', views.StudentMentorAPI.as_view(), name='student-mentor')
 ]
