23865b672fcf6f45ee144ffe06ec5fd485c54cc1
Archana-Bhamare <archusonar2@gmail.com>
2021-02-05 09:57:01 +0530

    Refactor : refactor the studenteducation serializer

--- learning_management/serializer.py
+++ learning_management/serializer.py
@@ -8,17 +8,21 @@ class AddCourseSerializer(serializers.ModelSerializer):
         fields = ['id', 'course_name', 'description']
         extra_kwargs = {'id': {'read_only': True}}
 
+
 class UpdateStudentDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['id','alternate_mobile_number', 'relation_with_alt_num', 'current_location', 'current_address', 'git_link',
+        fields = ['id', 'alternate_mobile_number', 'relation_with_alt_num', 'current_location', 'current_address',
+                  'git_link',
                   'year_of_exp']
         extra_kwargs = {'id': {'read_only': True}}
+
 
 class UpdateStudentEducationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Education
-        fields = '__all__'
+        fields = ['id','university','degree','degree_stream','percentage','from_date','till']
+        extra_kwargs = {'id': {'read_only': True}}
 
 class UpdateMentorDetailsSerializer(serializers.ModelSerializer):
     class Meta:
@@ -26,15 +30,16 @@ class UpdateMentorDetailsSerializer(serializers.ModelSerializer):
         fields = ['mentor', 'course']
         extra_kwargs = {'mentor': {'read_only': True}}
 
+
 class DisplayMentorCourseSerializer(serializers.ModelSerializer):
     course = serializers.StringRelatedField(read_only=True, many=True)
+
     class Meta:
         model = Mentor
         fields = ['mentor', 'course']
 
-class StudentMentorSerializer(serializers.ModelSerializer):
-    student = serializers.StringRelatedField(read_only=True)
 
+class StudentMentorSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentMentor
-        fields = ['student', 'mentor']
+        fields = ['student', 'mentor', 'course']
